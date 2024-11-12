c0d7f542e28b4f7f9494607076eedbb73d1fa1f4
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2024-11-11 17:49:50 -0800

    Completed exerices 1 and 2

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -22,48 +22,33 @@ export async function createExpenseServer(req: Request, res: Response, db: Datab
  
  } 
 
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
+export async function deleteExpense(req: Request, res: Response, db: Database) {
     // TO DO: Implement deleteExpense function
     const {id} = req.params; 
-  
 
-    if (!id) {
-        return res.status(400).send({ error: "Missing required field: id" });
-    }
+    try {
+        if (!id) {
+            return res.status(400).send({ error: "Missing required field: id" });
+        }
 
-    const expenseIndex = expenses.findIndex(expense => expense.id === id);
+        const result = await db.run('DELETE FROM expenses WHERE id = ?', id);
 
-   if (expenseIndex === -1) {
-       return res.status(404).send({ error: "Expense not found" });
-    }
+        if (result.changes === 0) {
+            return res.status(404).send({ error: "Expense not found" });
+        }
+        res.status(201).send({ result });
 
-    const deletedExpense = expenses.splice(expenseIndex, 1)[0]; 
-
-    res.status(200).send(deletedExpense);
+    } catch (error) {
+        return res.status(400).send({ error: `Could not delete expense, + ${error}` });
+    };
 }
 
 export async function getExpenses(req: Request, res: Response, db: Database) {
     
     try {
-        // Type casting the request body to the expected format.
-        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
- 
-        if (!description || !id || !cost) {
-            return res.status(400).send({ error: "Missing required fields" });
-        }
- 
-        await db.run('SELECT * FROM expenses');
-        res.status(200).send({ id, description, cost });
- 
+        const expenses = await db.all('SELECT * FROM expenses');
+        res.status(200).send({ expenses });
     } catch (error) {
- 
         return res.status(400).send({ error: `Could not get expenses, + ${error}` });
     };
-    
-    
-    
-    
-    
-    
-    res.status(200).send({ "data": expenses });
 }
