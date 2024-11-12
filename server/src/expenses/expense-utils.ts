a73ac022f9866292b66d98768ca08fb2f8d0969f
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2024-11-11 17:26:20 -0800

    Created database integration

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,22 +1,26 @@
+import { Database } from "sqlite";
 import { Expense } from "../types";
 import { Request, Response } from "express";
 
-export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
-    const { id, cost, description } = req.body;
+export async function createExpenseServer(req: Request, res: Response, db: Database) {
 
-    if (!description || !id || !cost) {
-        return res.status(400).send({ error: "Missing required fields" });
-    }
-
-    const newExpense: Expense = {
-        id: id,
-        description,
-        cost,
+    try {
+        // Type casting the request body to the expected format.
+        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
+ 
+        if (!description || !id || !cost) {
+            return res.status(400).send({ error: "Missing required fields" });
+        }
+ 
+        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
+        res.status(201).send({ id, description, cost });
+ 
+    } catch (error) {
+ 
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
-
-    expenses.push(newExpense);
-    res.status(201).send(newExpense);
-}
+ 
+ } 
 
 export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
     // TO DO: Implement deleteExpense function
@@ -38,6 +42,28 @@ export function deleteExpense(req: Request, res: Response, expenses: Expense[]) 
     res.status(200).send(deletedExpense);
 }
 
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
+export async function getExpenses(req: Request, res: Response, db: Database) {
+    
+    try {
+        // Type casting the request body to the expected format.
+        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
+ 
+        if (!description || !id || !cost) {
+            return res.status(400).send({ error: "Missing required fields" });
+        }
+ 
+        await db.run('SELECT * FROM expenses');
+        res.status(200).send({ id, description, cost });
+ 
+    } catch (error) {
+ 
+        return res.status(400).send({ error: `Could not get expenses, + ${error}` });
+    };
+    
+    
+    
+    
+    
+    
     res.status(200).send({ "data": expenses });
 }
